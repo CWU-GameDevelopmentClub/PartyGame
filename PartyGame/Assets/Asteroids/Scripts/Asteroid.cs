9baaeb1093c8001b6ecd025e236226e9b16bafc7
Miguel Garnica <mgarnica123@aol.com>
2019-04-25 21:29:56 -0700

    Asteroids!!
    Added:
        - Asteroid GameObject
        - Asteroid Script, Sprite, Prefab

---
+++ PartyGame/Assets/Asteroids/Scripts/Asteroid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private float speed = 3;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (rb.velocity.x == 0)
+        {
+            Move();
+        }
+        
+	}
+
+    public void Move()
+    {
+        rb.velocity = new Vector2(1, 1) * Random.Range(-1,3) * speed;
+    }
+}
