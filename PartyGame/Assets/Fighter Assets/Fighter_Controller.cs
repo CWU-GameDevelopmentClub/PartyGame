01c13efc48853d4b470a493dbf1f76e2a712eb42
KuraiSol <46945261+KuraiSol@users.noreply.github.com>
2019-05-03 15:50:23 -0700

    Simple arena and movement script made

---
+++ PartyGame/Assets/Fighter Assets/Fighter_Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter_Controller : MonoBehaviour
+{
+
+  public string hControl;
+  public string vControl;
+  public float speed;
+  
+
+  // Start is called before the first frame update
+  void Start()
+  {
+      
+  }
+
+  // Update is called once per frame
+  void Update(){
+    if (Input.GetAxis(hControl) != 0 || Input.GetAxis(vControl) != 0) {
+      transform.rotation = Quaternion.Euler(0, Mathf.Atan2(Input.GetAxis(hControl), Input.GetAxis(vControl)) * Mathf.Rad2Deg, 0);
+      transform.position += transform.forward * speed;
+    }
+  }
+}
