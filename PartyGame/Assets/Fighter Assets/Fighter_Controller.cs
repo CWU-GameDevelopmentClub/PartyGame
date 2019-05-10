14261938197e7992dc7a8cbf9b40e51a10a0fbf7
KuraiSol <46945261+KuraiSol@users.noreply.github.com>
2019-05-10 15:41:16 -0700

    cube fighter is now robo sumo, animator set up for this

--- PartyGame/Assets/Fighter Assets/Fighter_Controller.cs
+++ PartyGame/Assets/Fighter Assets/Fighter_Controller.cs
@@ -7,7 +7,11 @@ public class Fighter_Controller : MonoBehaviour
 
   public string hControl;
   public string vControl;
+  public string Attack;
+  public string Jump;
+  public string Defend;
   public float speed;
+  public Animator anim;
   
 
   // Start is called before the first frame update
@@ -19,8 +23,11 @@ public class Fighter_Controller : MonoBehaviour
   // Update is called once per frame
   void Update(){
     if (Input.GetAxis(hControl) != 0 || Input.GetAxis(vControl) != 0) {
-      transform.rotation = Quaternion.Euler(0, Mathf.Atan2(Input.GetAxis(hControl), Input.GetAxis(vControl)) * Mathf.Rad2Deg, 0);
-      transform.position += transform.forward * speed;
+      transform.rotation = Quaternion.Euler(0, Mathf.Atan2(Input.GetAxis(hControl), Input.GetAxis(vControl)), 0);
+      anim.Play("Walk");
     }
+
+
+
   }
 }
