640d9f391d035708081e13edb5e1f3ae2d936186
KuraiSol <46945261+KuraiSol@users.noreply.github.com>
2019-05-17 19:45:42 -0700

    moves through animations now

--- PartyGame/Assets/Fighter Assets/Fighter_Controller.cs
+++ PartyGame/Assets/Fighter Assets/Fighter_Controller.cs
@@ -12,6 +12,7 @@ public class Fighter_Controller : MonoBehaviour
   public string Defend;
   public float speed;
   public Animator anim;
+  public float charge;
   
 
   // Start is called before the first frame update
@@ -22,12 +23,22 @@ public class Fighter_Controller : MonoBehaviour
 
   // Update is called once per frame
   void Update(){
+    anim.SetInteger("speed", 0);
     if (Input.GetAxis(hControl) != 0 || Input.GetAxis(vControl) != 0) {
-      transform.rotation = Quaternion.Euler(0, Mathf.Atan2(Input.GetAxis(hControl), Input.GetAxis(vControl)), 0);
-      anim.Play("Walk");
+      transform.rotation = Quaternion.Euler(0, Mathf.Atan2(Input.GetAxis(hControl), Input.GetAxis(vControl)) * Mathf.Rad2Deg, 0);
+      anim.SetInteger("speed", 1);
     }
 
+    if (Input.GetKey(KeyCode.U)) {
+      charge += Time.deltaTime;
+    }
 
+    if (Input.GetKeyUp(KeyCode.I)) {
+      if (charge < .3f) {
+        anim.Play("Slap");
+      }
+      charge = 0;
+    }
 
   }
 }
