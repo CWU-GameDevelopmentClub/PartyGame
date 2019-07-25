948514e8d2e8c98442690619dfcee405da8f6475
Miguel Garnica <mgarnica123@aol.com>
2019-07-24 18:33:33 -0700

    Game_RoundTImer
    Added:
        - RoundTimer script
    Updated:
        - Score Manager prefabs (includes round timer UI text)

---
+++ PartyGame/Assets/Asteroids/Scripts/RoundTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class RoundTimer : MonoBehaviour {
+
+    private TextMeshProUGUI timerText;
+    public float roundTime = 60;
+
+	// Use this for initialization
+	void Start () {
+        timerText = GetComponent<TextMeshProUGUI>();    
+        timerText.text = "Timer: "; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        roundTime -=  Time.deltaTime;
+        timerText.text = "Timer: " + (int) roundTime;
+
+        if(roundTime <= 0)
+        {
+            timerText.text = "END GAME";
+        }
+	}
+}
